11569a97a1ba0c9830a47739c2c6d3909778ee7d
Keenan <Keenanking555@gmail.com>
2019-02-04 13:32:06 -0700

    frame has been begun, but still needs a ton of work

---
+++ src/pokemon/view/PokedexFrame.java
@@ -0,0 +1,18 @@
+package pokemon.view;
+
+import pokemon.controller.PokedexController;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+import chat.controller.IOController;
+import chat.controller.chatbotController;
+import pokemon.controller.PokedexController;
+
+public class PokedexFrame
+{
+	
+}
